dac84165fab1f9ad27069aafc6db1bfb2d8d08f5
Kumi Taha A <kumitaha123@gmail.com>
2023-09-17 14:54:02 +0500

    Changed Title

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Gallery',
+      title: " GIT Gallery APP",
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
